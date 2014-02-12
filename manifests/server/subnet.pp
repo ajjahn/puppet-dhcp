1402672a98aa4e6d8df308b2597b15c4cda853d1
George Brown <george.brown@cscs.ch>
2014-02-12 11:47:32 +0100

    Made manifests more compliant to the puppetlabs style guide
    Before
    
    puppet-lint * | wc -l
    88
    
    After
    
    puppet-lint  --with-filename *
    manifests/server.pp - WARNING: class not documented on line 1
    manifests/init.pp - WARNING: class not documented on line 1
    manifests/server/service.pp - WARNING: class not documented on line 1
    manifests/server/subnet.pp - WARNING: selector inside resource block on line 30
    manifests/server/subnet.pp - WARNING: defined type not documented on line 1
    manifests/server/config.pp - WARNING: class not documented on line 1
    manifests/server/host.pp - WARNING: selector inside resource block on line 23
    manifests/server/host.pp - WARNING: defined type not documented on line 1
    manifests/server/install.pp - WARNING: class not documented on line 1
    
    Outstanding items are around documentation and selectors within resource blocks,
    I felt this was beyond the scope of this simple update.

--- manifests/server/subnet.pp
+++ manifests/server/subnet.pp
@@ -1,37 +1,37 @@
-define dhcp::server::subnet ($ensure=present,
-                            $broadcast,
+define dhcp::server::subnet ($broadcast,
                             $netmask,
                             $routers,
                             $range_begin,
                             $range_end,
+                            $ensure=present,
                             $dns_servers=false,
                             $subnet_mask=false,
                             $domain_name=false,
                             $other_opts=false,
                             $next_server=false,
                             $filename=false) {
-  
+
   $subnet = $name
-  $context = '/files/etc/dhcp/dhcpd.conf'   
+  $context = '/files/etc/dhcp/dhcpd.conf'
   $config = "/etc/dhcp/subnets/${name}.conf"
   $include = "include[. = '${config}']"
-             
-  file { "${config}":
-    ensure => $ensure,
-    owner => root,
-    group => root,
+
+  file { $config:
+    ensure  => $ensure,
+    owner   => root,
+    group   => root,
     content => template("${module_name}/subnet.conf.erb"),
-    require => Class["dhcp::server::config"],
-    notify => Class["dhcp::server::service"]
+    require => Class['dhcp::server::config'],
+    notify  => Class['dhcp::server::service']
   }
-  
+
   augeas { "include-${name}.conf":
-		context => $context,
-		changes => $ensure ? {
-		  present => "set ${include} ${config}",
-		  default => "rm ${include}",
-		},
-		require => File["${config}"]
-	}
-  
+    context => $context,
+    changes => $ensure ? {
+      present => "set ${include} ${config}",
+      default => "rm ${include}",
+      },
+    require => File[$config]
+  }
+
 }
